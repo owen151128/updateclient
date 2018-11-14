98c71797f387074b8089b98d3011224399f6df4e
owen151128 <owen151128@gmail.com>
2018-11-14 15:39:41 +0900

    Server Connector 수정
    updateInfo Tree 자료 구조 수정된 사항 반영

--- src/main/java/network/ServerConnector.java
+++ src/main/java/network/ServerConnector.java
@@ -1,13 +1,13 @@
 package network;
 
+import model.UpdateInfoDTO;
 import model.UpdateInfoTree;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.OutputStreamWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
 
 /**
  * 서버와 통신을 담당하는 클래스
@@ -16,18 +16,19 @@ import java.net.UnknownHostException;
  */
 public class ServerConnector {
 
-    private static final String REQUEST_UPDATE_INFO_TREE = "updateInfoTree";
+    private static final String REQUEST_UPDATE_INFO_TREE = "updateInfoDTO";
     private static final String NEW_LINE = "\n";
 
     private static final String MSG_TRY_CONNECT_SERVER = "connecting to update server ...";
     private static final String MSG_CONNECTED_SERVER = "connected update server";
-    private static final String MSG_SEND_UPDATE_TREE_REQUEST = "send UpdateInfoTree download request";
-    private static final String MSG_DOWNLOAD_UPDATE_TREE = "download UpdateInfoTree...";
-    private static final String MSG_UPDATE_TREE_DOWNLOADED = "download UpdateInfoTree complete";
+    private static final String MSG_SEND_UPDATE_TREE_REQUEST = "send UpdateInfoDTO download request";
+    private static final String MSG_DOWNLOAD_UPDATE_TREE = "download UpdateInfoDTO...";
+    private static final String MSG_UPDATE_TREE_DOWNLOADED = "download UpdateInfoDTO complete";
     private static final String MSG_DISCONNECTED_SERVER = "Disconnected from update server";
 
     private static final String ERR_SERVER_NOT_FOUND = "server not found. check IP or Network state";
     private static final String ERR_UPDATE_INFO_TREE_CLASS_NOT_FOUND = "UpdateInfoTree class not found. check Java class";
+    private static final String ERR_CONNECTION_TIME_OUT = "server connection time out. check Network state";
     private static final String ERR_IO_FAILED = "I/O failed. check stack trace";
     private static final String ERR_STREAM_CLOSED_FAILED = "Stream close Failed! check stack trace";
 
@@ -58,21 +59,23 @@ public class ServerConnector {
     }
 
     /**
-     * Update Tree Node 를 서버로 부터 받아오는 메소드
+     * Update Info DTO 를 서버로 부터 받아오는 메소드
      *
      * @param serverIP String 형태의 서버 IP
      * @param port     int 형태의 서버 port 번호
-     * @return UpdateInfoTree 형태의 서버 에서 받아온 업데이트 정보
+     * @return Update Info DTO 형태의 서버 에서 받아온 업데이트 정보
      */
-    public UpdateInfoTree getUpdateTreeNode(String serverIP, int port) {
+    public UpdateInfoDTO getUpdateInfoDTO(String serverIP, int port, int timeout) {
 
-        UpdateInfoTree updateInfoTree = null;
+        UpdateInfoDTO result = null;
 
         try {
 
             System.out.println(MSG_TRY_CONNECT_SERVER);
-
-            socket = new Socket(serverIP, port);
+            SocketAddress target = new InetSocketAddress(serverIP, port);
+            socket = new Socket();
+            socket.setSoTimeout(timeout * 1000);
+            socket.connect(target, timeout * 1000);
 
             osw = new OutputStreamWriter(socket.getOutputStream());
             bw = new BufferedWriter(osw);
@@ -88,7 +91,7 @@ public class ServerConnector {
 
             System.out.println(MSG_DOWNLOAD_UPDATE_TREE);
 
-            updateInfoTree = (UpdateInfoTree) ois.readObject();
+            result = (UpdateInfoDTO) ois.readObject();
 
             System.out.println(MSG_UPDATE_TREE_DOWNLOADED);
 
@@ -101,6 +104,10 @@ public class ServerConnector {
             e.printStackTrace();
             System.out.println(ERR_UPDATE_INFO_TREE_CLASS_NOT_FOUND);
 
+        } catch (SocketTimeoutException e) {
+
+            System.out.println(ERR_CONNECTION_TIME_OUT);
+
         } catch (IOException e) {
 
             e.printStackTrace();
@@ -109,6 +116,10 @@ public class ServerConnector {
         } finally {
 
             try {
+
+                if (socket.isConnected()) {
+                    System.out.println(MSG_DISCONNECTED_SERVER);
+                }
 
                 if (socket != null) {
                     socket.close();
@@ -126,8 +137,6 @@ public class ServerConnector {
                     ois.close();
                 }
 
-                System.out.println(MSG_DISCONNECTED_SERVER);
-
             } catch (IOException e) {
 
                 e.printStackTrace();
@@ -136,6 +145,6 @@ public class ServerConnector {
             }
         }
 
-        return updateInfoTree;
+        return result;
     }
 }
